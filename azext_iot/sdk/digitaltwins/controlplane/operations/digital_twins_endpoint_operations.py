c0f57bce8654234811f18666beddd402bf784109
Ryan K <ryan.k@outlook.com>
2020-10-12 14:51:40 -0700

    Regenerated Control-Plane SDK from updated 2020-10-31 swagger

--- azext_iot/sdk/digitaltwins/controlplane/operations/digital_twins_endpoint_operations.py
+++ azext_iot/sdk/digitaltwins/controlplane/operations/digital_twins_endpoint_operations.py
@@ -175,7 +175,7 @@ class DigitalTwinsEndpointOperations(object):
 
 
     def _create_or_update_initial(
-            self, resource_group_name, resource_name, endpoint_name, properties=None, custom_headers=None, raw=False, **operation_config):
+            self, resource_group_name, resource_name, endpoint_name, properties, custom_headers=None, raw=False, **operation_config):
         endpoint_description = models.DigitalTwinsEndpointResource(properties=properties)
 
         # Construct URL
@@ -227,7 +227,7 @@ class DigitalTwinsEndpointOperations(object):
         return deserialized
 
     def create_or_update(
-            self, resource_group_name, resource_name, endpoint_name, properties=None, custom_headers=None, raw=False, polling=True, **operation_config):
+            self, resource_group_name, resource_name, endpoint_name, properties, custom_headers=None, raw=False, polling=True, **operation_config):
         """Create or update DigitalTwinsInstance endpoint.
 
         :param resource_group_name: The name of the resource group that
